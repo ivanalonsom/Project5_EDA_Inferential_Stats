46f52a77c35a5f6988faea5c10f0079a9ff09063
ivanalonsom <ivan7yt@gmail.com>
2024-09-25 14:34:11 +0200

    inicioDia3

--- functions.py
+++ functions.py
@@ -14,3 +14,18 @@ def drop_na_df(df, col):
     import pandas as pd
 
     return df.dropna(subset=[col])
+
+
+def outlier_slayer(data): # automatically removes outliers based on Q1, Q3
+    import numpy as np
+    """
+    Automatically removes outliers based on Q1, Q3
+    """
+    for column in data.select_dtypes(include=[np.number]):
+        Q1 = data[column].quantile(0.25)
+        Q3 = data[column].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        data = data[(data[column] >= lower_bound) & (data[column] <= upper_bound)]
+    return data
